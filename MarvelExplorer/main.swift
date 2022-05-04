7a25e87a98111758464b83bbdad0e2213afc9239
Mostfa Essam <almme111@gmail.com>
2022-05-04 05:31:30 +0200

    Fixed main not tracked by git

---
+++ MarvelExplorer/main.swift
@@ -0,0 +1,7 @@
+
+import UIKit
+
+private let fakeAppDelegateClass: AnyClass? = NSClassFromString("MarvelExplorerTests.FakeAppDelegate")
+private let appDelegateClass: AnyClass = fakeAppDelegateClass ?? AppDelegate.self
+
+_ = UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(appDelegateClass))
