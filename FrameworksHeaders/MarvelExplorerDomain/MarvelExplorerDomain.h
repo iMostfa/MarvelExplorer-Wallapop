5d424861c4188072181a9cb3ab2d73caae9f8957
Mostfa Essam <almme111@gmail.com>
2022-05-05 18:55:42 +0200

    Added frameworks headers to git

---
+++ FrameworksHeaders/MarvelExplorerDomain/MarvelExplorerDomain.h
@@ -0,0 +1,18 @@
+//
+//  MarvelExplorerDomain.h
+//  MarvelExplorerDomain
+//
+//  Created by Mostfa on 05/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarvelExplorerDomain.
+FOUNDATION_EXPORT double MarvelExplorerDomainVersionNumber;
+
+//! Project version string for MarvelExplorerDomain.
+FOUNDATION_EXPORT const unsigned char MarvelExplorerDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarvelExplorerDomain/PublicHeader.h>
+
+
---
+++ FrameworksHeaders/MarvelExplorerShared/MarvelExplorerShared.h
@@ -0,0 +1,16 @@
+//
+//  MarvelExplorerShared.h
+//  MarvelExplorerShared
+//
+//  Created by Mostfa on 05/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarvelExplorerShared.
+FOUNDATION_EXPORT double MarvelExplorerSharedVersionNumber;
+
+//! Project version string for MarvelExplorerShared.
+FOUNDATION_EXPORT const unsigned char MarvelExplorerSharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarvelExplorerShared/PublicHeader.h>
